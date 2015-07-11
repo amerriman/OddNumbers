ccab86025cf7c5f8e0df741d1875c1548177d994
amerriman <amm434@gmail.com>
2015-07-11 11:41:11 -0600

    include solution

---
+++ OddNumbers.js
@@ -0,0 +1,14 @@
+//as a for loop
+
+for(num = 1; num < 1337; num++){
+    if(num % 2 !== 0){
+console.log(num);}
+}
+
+//as a while loop
+var num = 1;
+while (num <1338) {
+    if (num %2 !==0)
+    console.log(num);
+    num++;
+}
